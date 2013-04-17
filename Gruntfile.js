b63457d60c7f01cf1400c0753a6c2824b3943970
Chris Bumgardner <cbumgard@gmail.com>
2013-04-17 00:16:23 -0700

    Grunt now does not specify individual custom or 3rd party client libs -- it uses them as defined in the assets/ folder (custom) or in component.json (3rd party) so new ones can be added without modifying the grunt file

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,26 @@
+var fs = require('fs');
+
 module.exports = function(grunt) {
   // Define root paths to 3party assets and this app's custom assets:
   var gruntBowerDir = './grunt-bower-lib';
   var customAssetsDir = './assets';
+
+  // Order critical list of 3rd party & custom dependencies,
+  // list in correct order (i.e. jQuery first for 3rd party)
+  // and paths relative to the Bower ./components directory.
+  // CRITICAL: Make sure jQuery is first in your components.json file.
+  // Also TODO: if there is an order dependency WITHIN a components 
+  // directory (for example jquery.validation has an additional-methods.js
+  // that requires jquery.validate.js to load first), need a way
+  // to manually override in that case. Ignoring for now and not using
+  // components with those inner dependencies.
+  var bowerJs = [];
+  var bowerCss = [];
+  var bowerComps = grunt.file.readJSON('component.json');
+  Object.keys(bowerComps.dependencies).forEach(function(comp) {
+    bowerJs.push(gruntBowerDir + '/' + comp + '/*.js');
+    bowerCss.push(gruntBowerDir + '/' + comp + '/*.css');
+  });
 
   // Initialize Grunt configuration:
   grunt.initConfig({
@@ -32,35 +51,22 @@ module.exports = function(grunt) {
           '<%= grunt.template.today("yyyy-mm-dd") %> */'
       },
       '3party-js': {
-        src: [
-          gruntBowerDir + '/jquery/jquery.js',
-          gruntBowerDir + '/jquery.validation/jquery.validate.js',
-          gruntBowerDir + '/jquery.validation/additional-methods.js',
-          gruntBowerDir + '/jquery.scrollTo/jquery.scrollTo.js',        
-          gruntBowerDir + '/bootstrap/bootstrap.js'
-        ],
+        src: bowerJs,
         dest: gruntBowerDir + '/3party/3party.js'
       },
       '3party-css': {
-        src: [
-          gruntBowerDir + '/bootstrap/bootstrap.css'
-        ],
+        src: bowerCss,
         dest: gruntBowerDir + '/3party/3party.css'
       },
       'custom-js': {
         src: [
-          customAssetsDir + '/js/common.js',
-          customAssetsDir + '/js/home-page.js',
-          customAssetsDir + '/js/signup.js'
+          customAssetsDir + '/js/*.js'
         ],
         dest: gruntBowerDir + '/<%= pkg.name %>/<%= pkg.name %>.js'
       },
       'custom-css': {
         src: [
-          customAssetsDir + '/css/common.css',
-          customAssetsDir + '/css/home-page.css',
-          customAssetsDir + '/css/signup.css',
-          customAssetsDir + '/css/nav-bar.css'
+          customAssetsDir + '/css/*.css'
         ],
         dest: gruntBowerDir + '/<%= pkg.name %>/<%= pkg.name %>.css'
       }            
